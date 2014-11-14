5b2afd6511eee417576e333f11a8fa76aeb4fa34
Calyhre <charley.david@wopata.com>
2014-11-14 14:18:17 +0100

    Fix typo

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var http = require('http');
 var hangoutsBot = require("hangouts-bot");
 var dotenv = require('dotenv').load();
 
-var bot = new hangoutsBot(process.env.HANGOUT_USER, process.env.HANGOUT_PASSWORD);
+var bot = new hangoutsBot(process.env.HANGOUTS_USER, process.env.HANGOUTS_PASSWORD);
 
-if(!process.env.HANGOUT_USER || !process.env.HANGOUT_PASSWORD){
-  throw 'Missing HANGOUT_USER and/or HANGOUT_USER !';
+if(!process.env.HANGOUTS_USER || !process.env.HANGOUTS_PASSWORD){
+  throw 'Missing HANGOUTS_USER and/or HANGOUTS_USER !';
 }
 
-var autoReplyMessage = process.env.HANGOUT_MESSAGE || "I'm not available on Hangouts, and I may never see this message. Please send me an email.";
+var autoReplyMessage = process.env.HANGOUTS_MESSAGE || "I'm not available on Hangouts, and I may never see this message. Please send me an email.";
 
 bot.on('online', function() {
   console.log('online');
